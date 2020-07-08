91f5e791003804aa4aab96beedbe7aca0f451303
Murali Krishnan A <amuralikrishnan@outlook.com>
2020-07-08 22:51:11 +0530

    Fixed last messyroom not redirecting to image puzzle issue

--- res/messyroom/messyroom.js
+++ res/messyroom/messyroom.js
@@ -1,12 +1,17 @@
-var winH = 0, winW = 0, viewSpan, pers = 700, backwall, scratch = true;
+var winH = 0,
+  winW = 0,
+  viewSpan,
+  pers = 700,
+  backwall,
+  scratch = true;
 
 let init = () => {
-  document.ondragstart = _ => false;
+  document.ondragstart = (_) => false;
   winH = window.innerHeight;
   winW = window.innerWidth;
   viewSpan = document.getElementById("viewSpan");
   backwall = document.getElementById("backwall");
-  repositionBackface(); 
+  repositionBackface();
   initClock();
   initNewspaper();
 };
@@ -17,9 +22,9 @@ let begin = () => {
   let introDiv = document.getElementById("c-body");
 
   button.onclick = () => {
-      introDiv.classList.add("hidden");
-  }
-}
+    introDiv.classList.add("hidden");
+  };
+};
 
 document.addEventListener("DOMContentLoaded", begin);
 
@@ -28,7 +33,7 @@ let setupMirror = () => {
   mirror.addEventListener("click", () => {
     displaySuccessMessage();
   });
-}
+};
 
 let initNewspaper = () => {
   let newspaper = document.getElementById("newspaper");
@@ -38,7 +43,7 @@ let initNewspaper = () => {
   newspaper.addEventListener("click", () => {
     configNewsPaper("overlay");
   });
-}
+};
 
 let configNewsPaper = (size = "ord") => {
   let newspaper = document.getElementById("newspaper");
@@ -61,58 +66,67 @@ let configNewsPaper = (size = "ord") => {
     newspaper.style.width = "";
     newspaper.style.height = "";
   }
-}
+};
 
 let repositionBackface = () => {
   var x = 0;
-  if(winW < winH){
+  if (winW < winH) {
     x = winW;
   } else {
     x = winH;
   }
   backwall.style.transform = "translateZ(-" + (x - 1) + "px)";
   backwall.style.webkitTransform = "translateZ(-" + (x - 1) + "px)";
-}
+};
 
-let updatePerspective = (originX,originY) => {
-    let viewSpan = document.getElementById("viewSpan")
-    viewSpan.style.perspectiveOrigin = originX.toFixed(4) + "% " + originY.toFixed(4) + "%";
-    viewSpan.style.webkitPerspectiveOrigin = originX.toFixed(4) + "% " + originY.toFixed(4) + "%";
-}
+let updatePerspective = (originX, originY) => {
+  let viewSpan = document.getElementById("viewSpan");
+  viewSpan.style.perspectiveOrigin =
+    originX.toFixed(4) + "% " + originY.toFixed(4) + "%";
+  viewSpan.style.webkitPerspectiveOrigin =
+    originX.toFixed(4) + "% " + originY.toFixed(4) + "%";
+};
 
 let initClock = () => {
   let secondsHand = document.getElementsByClassName("seconds-hand")[0];
   let minutesHand = document.getElementsByClassName("min-hand")[0];
   let hoursHand = document.getElementsByClassName("hour-hand")[0];
-  
-  setInterval( _ => {
-      setDate(secondsHand, minutesHand, hoursHand);
+
+  setInterval((_) => {
+    setDate(secondsHand, minutesHand, hoursHand);
   }, 1000);
-}
+};
 
 let setDate = (secondsHand, minutesHand, hoursHand) => {
   const now = new Date();
   const seconds = now.getSeconds();
-  const secondsDeg = ((seconds / 60) * 360) + 90;
+  const secondsDeg = (seconds / 60) * 360 + 90;
   secondsHand.style.transform = `translateY(-2px) rotate(${secondsDeg}deg)`;
 
   const mins = now.getMinutes();
-  const minsDeg = ((mins / 60) * 360) + 90;
+  const minsDeg = (mins / 60) * 360 + 90;
   minutesHand.style.transform = `translateY(-3.5px) rotate(${minsDeg}deg)`;
 
   const hours = now.getHours();
-  const hoursDeg = ((hours / 12) * 360) + 90;
+  const hoursDeg = (hours / 12) * 360 + 90;
   hoursHand.style.transform = `translateY(-5px) rotate(${hoursDeg}deg)`;
-}
+};
 
 let displaySuccessMessage = () => {
   let introDiv = document.getElementById("c-body");
-  let successCode = '<h2 class="c-scroll__heading">Look around!!!</h2><br><p class="c-scroll__paragraph">Dyuksha\'s On...</p> '
-  + '<br><p class="c-scroll__paragraph" style="font-family:\'baseFont\';">Please wait while next level is being prepared... Do not reload the page...</p>';
+  let successCode =
+    '<h2 class="c-scroll__heading">Look around!!!</h2><br><p class="c-scroll__paragraph">Dyuksha\'s On...</p> ' +
+    '<br><p class="c-scroll__paragraph" style="font-family:\'baseFont\';">Please wait while next level is being prepared... Do not reload the page...</p>';
   introDiv.getElementsByClassName("c-scroll__text")[0].innerHTML = successCode;
 
   introDiv.classList.remove("hidden");
 
   // redirect to next page
   redirect();
-}
+};
+
+let redirect = () => {
+  setTimeout(() => {
+    window.location.href = "../imagePuzzle/imagePuzzle.html";
+  }, 1000);
+};
